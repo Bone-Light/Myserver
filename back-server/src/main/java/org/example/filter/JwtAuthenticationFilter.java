1db862ab10f51b6834bf78631b510231515ccbd2
GuoRui <G.163.com>
2025-03-26 13:10:44 +0800

    Filter 一部分，数据更新

--- back-server/src/main/java/org/example/filter/JwtAuthenticationFilter.java
+++ back-server/src/main/java/org/example/filter/JwtAuthenticationFilter.java
@@ -1,11 +1,24 @@
 package org.example.filter;
 
+import com.auth0.jwt.interfaces.DecodedJWT;
 import jakarta.annotation.Resource;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.entity.RestBean;
 import org.example.utils.Const;
 import org.example.utils.JwtUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 @Component
 public class JwtAuthenticationFilter {
@@ -13,12 +26,60 @@ public class JwtAuthenticationFilter {
     @Resource
     JwtUtils jwtUtils;
 
+    //@Resource
+//    ClientServer
+    //@Resource
+//    AccountService accountService
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+        String authorization = request.getHeader("Authorization");
+        String url = request.getRequestURI();
+        if(url.startsWith("/monitor")){
+            if(!url.endsWith("/register")){
+                // 自建
+                Client client =  clientService.findCilentByToken(authorization);
+                if(client != null){
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    response.setCharacterEncoding("UTF-8");
+                    response.getWriter().write(RestBean.failure(401, "未注册").asJsonString());
+                    return;
+                } else {
+                    request.setAttribute(Const.ATTR_CLIENT, client);
+                }
+            }
+        } else {
+            DecodedJWT jwt = jwtUtils.resolveToken(authorization);
+            if(jwt != null){
+                UserDetails userDetails = jwtUtils.toUser(jwt);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                request.setAttribute(Const.ATTR_USER_ID, jwtUtils.toId(jwt));
+                request.setAttribute(Const.ATTR_USER_ROLE, new ArrayList<>(userDetails.getAuthorities()).getFirst().getAuthority());
+
+                if(request.getRequestURI().startsWith("/terminal") && !accessShell(
+                        (int) request.getAttribute(Const.ATTR_USER_ID),
+                        (String) request.getAttribute(Const.ATTR_USER_ROLE),
+                        (int) request.getAttribute(Const.ATTR_CLIENT)
+                )){
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    response.setCharacterEncoding("UTF-8");
+                    response.getWriter().write(RestBean.failure(401, "权限不足").asJsonString());
+                    return;
+                }
+            }
+        }
+
+        chain.doFilter(request,response);
+    }
+
     private boolean accessShell(int userId, String userRole, int clientId){
         if(Const.ROLE_ADMIN.equals(userRole.substring(5))){
             return true;
         } else {
-            Account account = ....;
-            return account.get ....;
+            Account account = accountService.getById(userId);
+            return account.getClientList().contains(clientId);
         }
     }
 }
